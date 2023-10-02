390c1debf75d99e47796ca31e007585e30074c12
Ataberk ÖZKAR <82778256+ataberkozkarr@users.noreply.github.com>
2023-10-02 11:46:51 +0300

    Add files via upload

---
+++ Day4/Design/Design/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Design
+//
+//  Created by Ataberk Özkar on 30.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Pizza"
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named: "main_color")
+        appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "writing_color")!, .font: UIFont(name: "Pacifico-Regular", size: 22)!]
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+    }
+
+
+}
+
